d54e4b78633ae21d8c8725783f4bfeb0ab8f829a
Duca Tudor <duca.2001@mail.ru>
2024-04-20 14:10:01 +0300

    Removed unused entities

--- src/main/webapp/app/admin/admin-routing.module.ts
+++ src/main/webapp/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 /* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
 
 @NgModule({
@@ -10,31 +11,12 @@ import { RouterModule } from '@angular/router';
         path: 'user-management',
         loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule),
         data: {
-          pageTitle: 'userManagement.home.title',
-        },
-      },
-      {
-        path: 'docs',
-        loadChildren: () => import('./docs/docs.module').then(m => m.DocsModule),
-      },
-      {
-        path: 'configuration',
-        loadChildren: () => import('./configuration/configuration.module').then(m => m.ConfigurationModule),
-      },
-      {
-        path: 'health',
-        loadChildren: () => import('./health/health.module').then(m => m.HealthModule),
-      },
-      {
-        path: 'logs',
-        loadChildren: () => import('./logs/logs.module').then(m => m.LogsModule),
-      },
-      {
-        path: 'metrics',
-        loadChildren: () => import('./metrics/metrics.module').then(m => m.MetricsModule),
-      },
+          pageTitle: 'userManagement.home.title'
+        }
+      }
       /* jhipster-needle-add-admin-route - JHipster will add admin routes here */
-    ]),
-  ],
+    ])
+  ]
 })
-export class AdminRoutingModule {}
+export class AdminRoutingModule {
+}
