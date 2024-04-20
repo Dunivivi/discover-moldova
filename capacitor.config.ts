76e6eaea86d19a855d807624f2ed82bd3de1de5c
Duca Tudor <duca.2001@mail.ru>
2024-04-20 13:20:28 +0300

    IOS & Android convertor added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'travel',
+  webDir: 'target/classes/static'
+};
+
+export default config;
